9fe16bc4a9b5263e66f936ba77822ccd1c99b654
Daniyel Tkachuk <danilaradyn7357899@gmail.com>
2024-09-14 21:42:49 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,119 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+
+
+const SYMBOL_X = "X";
+const SYMBOL_O = "O";
+
+const computeWinner = (cells) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+
+    if (
+      cells[a] &&
+      cells[a] === cells[b] &&
+      cells[a] === cells[c]
+    ) {
+      return [a, b, c]
+    }
+  }
+}
 
 function App() {
+  const [currentStep, setCurrentStep] = React.useState(SYMBOL_O);
+  const [cells, setCells] = React.useState([
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
+  const [winnerSequence, setWinnerSequence] = React.useState(undefined);
+
+  const getSymbolClassName = (symbol) => {
+    if (symbol === SYMBOL_O) return "symbol--o";
+    if (symbol === SYMBOL_X) return "symbol--x";
+    return "";
+  }
+
+  const renderSymbol = (symbol) => {
+    return (
+      <span className={`symbol ${getSymbolClassName(symbol)}`}>
+        {symbol}
+      </span>
+    )
+  }
+
+  const onClickHandler = (index) => {
+    if (cells[index] || winnerSequence) return;
+
+    const cellsCopy = cells.slice();
+    cellsCopy[index] = currentStep;
+
+    const winner = computeWinner(cellsCopy)
+
+    setCells(cellsCopy);
+    setCurrentStep(currentStep === SYMBOL_O ? SYMBOL_X : SYMBOL_O);
+    setWinnerSequence(winner);
+  }
+
+  const restartGame = () => {
+    setCells(cells.map(() => null));
+    setWinnerSequence(undefined);
+  }
+
+  const winnerSymbol = winnerSequence ? cells[winnerSequence[0]] : undefined;
+
+  const gameStatusMessage = () => {
+    if (winnerSequence) {
+      return <>Победитель: {renderSymbol(winnerSymbol)}</>
+    }
+    if (cells.every(el => el !== null)) {
+      return <>Победила дружба!</>
+    }
+
+    return <>Ход: {renderSymbol(currentStep)}</>
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game">
+      <div className="game-info">
+        {gameStatusMessage()}
+      </div>
+      <div className="game-field">
+        {
+          cells
+          && cells.map((symbol, index) => {
+            const isWinner = winnerSequence?.includes(index);
+            return (
+              <button
+                key={index}
+                className={`cell ${isWinner ? "cell--win" : ""}`}
+                onClick={() => onClickHandler(index)}
+              >
+                {symbol ? renderSymbol(symbol) : null}
+              </button>
+            )
+          })
+        }
+      </div>
+      <button className="restart" onClick={restartGame}>сбросить</button>
     </div>
   );
 }
